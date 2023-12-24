4672f4e8df59970158e653660e343a99b6d8770c
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-24 23:42:13 +0530

    Update App.js

--- ui/ src/App.js
+++ ui/ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 
+const servicePorts = {
+  biryani: 3001, // Update with actual host port
+  pizza: 3002,   // Update with actual host port
+  snacks: 3003,  // Update with actual host port
+  cakes: 3004    // Update with actual host port
+};
+
 function App() {
   const [menu, setMenu] = useState([]);
 
   const fetchMenu = async (service) => {
-    const response = await fetch(`http://13.127.193.217:PORT/${service}`);
+    const port = servicePorts[service];
+    const response = await fetch(`http://13.127.193.217:${servicePorts[service]}/${service}`);
     const data = await response.json();
     setMenu(data);
   };
@@ -13,7 +21,7 @@ function App() {
     <div>
       <h1>Welcome to Rajinikanth Kitchen</h1>
       <button onClick={() => fetchMenu('biryani')}>Biryani</button>
-      {/* Repeat for Pizza, Snacks, Cakes */}
+      {/* Repeat buttons for Pizza, Snacks, Cakes */}
       <ul>
         {menu.map((item, index) => <li key={index}>{item.name} - {item.price}</li>)}
       </ul>
