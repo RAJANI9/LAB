48b342061906482050f2fb579aaac25beb7a6cbe
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-24 20:53:27 +0530

    Create App.js

---
+++ ui/ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [menu, setMenu] = useState([]);
+
+  const fetchMenu = async (service) => {
+    const response = await fetch(`http://localhost:PORT/${service}`);
+    const data = await response.json();
+    setMenu(data);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Rajinikanth Kitchen</h1>
+      <button onClick={() => fetchMenu('biryani')}>Biryani</button>
+      {/* Repeat for Pizza, Snacks, Cakes */}
+      <ul>
+        {menu.map((item, index) => <li key={index}>{item.name} - {item.price}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
